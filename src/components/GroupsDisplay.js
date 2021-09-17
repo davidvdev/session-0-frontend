5a828f6aeba91a19837e40b545650a0b8b05eb42
davidvdev <david@davidvdev.com>
2021-09-17 18:43:39 -0500

    fixed Search rendering key warning

--- src/components/GroupsDisplay.js
+++ src/components/GroupsDisplay.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const GroupsDisplay = ({ group }) => {
     return(
-        <Link to={`/group/${group.ref['@ref'].id}`}>
-            <div className="card-small" key={group.ref['@ref'].id}>
+        <div className="card-small" key={group.ref['@ref'].id}>
+            <Link to={`/group/${group.ref['@ref'].id}`}>
                 <div className="left">
                     <img src={group.data.bannerImg} alt="group emblem"/>
                     <h3>{group.data.groupName}</h3>
@@ -13,8 +13,8 @@ const GroupsDisplay = ({ group }) => {
                     <h4>{group.data.gameInfo}</h4>
                     <h5>{group.data.gm}</h5>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 

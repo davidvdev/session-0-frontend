50889ef4719f80ed4b053971e3a7458d1ad7ed0a
davidvdev <david@davidvdev.com>
2021-09-20 22:01:35 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const login = async (formData) => {
         body: JSON.stringify(formData)
     })
     const data = await response.json()
-    if (data.message.includes("failed")){
+    if (data.message !== undefined){
         alert("Password/Email combination incorrect.")
     } else {
         setUserAuth(data)
--- src/components/LogoSplash.js
+++ src/components/LogoSplash.js
@@ -4,7 +4,7 @@ const LogoSplash = () => {
     return (
         <div>
             <h1>Session 0</h1>
-            <img class="logo-img" src="android-chrome-192x192.png" alt="d10 logo"/>
+            <img className="logo-img" src="android-chrome-192x192.png" alt="d10 logo"/>
             <h2>Find Players - Find Games</h2>
 	        <h3>Adventure Awaits!</h3>
         </div>

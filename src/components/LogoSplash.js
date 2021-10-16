b93fa5af3a07795de6014722cb107ead995934ce
Jacob Stephens <jacob@stewardgoods.com>
2021-10-16 15:23:15 -0400

    add intro paragraph to welcome view

--- src/components/LogoSplash.js
+++ src/components/LogoSplash.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-const LogoSplash = () => {
-    return (
-        <div>
-            <h1>Session 0</h1>
-            <img className="logo-img" src="android-chrome-192x192.png" alt="d10 logo"/>
-            <h2>Find Players - Find Games</h2>
-	        <h3>Adventure Awaits!</h3>
-        </div>
-    )
-}
+const LogoSplash = (props) => {
+  return (
+    <div>
+      <h1>Session 0</h1>
+      <img
+        className="logo-img"
+        src="android-chrome-192x192.png"
+        alt="d10 logo"
+      />
+      <h2>Find Players - Find Games</h2>
+      <h3>Adventure Awaits!</h3>
+      <p className="intro-paragraph">{props.introParagraph}</p>
+    </div>
+  );
+};
 
-export default LogoSplash
+export default LogoSplash;
